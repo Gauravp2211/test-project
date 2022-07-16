5dcd9dff4504bb41aeb9feb90b8d860287010064
gauravp2211 <gauravpatne11@gmail.com>
2022-07-17 00:26:52 +0530

    first updates

---
+++ src/firstprojectofauagbatch/Firstoftheaugbatch.java
@@ -0,0 +1,17 @@
+package firstprojectofauagbatch;
+
+public class Firstoftheaugbatch {
+
+	public static void main(String[] args) {
+	
+    int i=100;
+    while(i>=10)
+    	
+    {	    	System.out.print(i + "\t");
+    	
+    	i--;
+    }
+    	
+    }
+}
+    
